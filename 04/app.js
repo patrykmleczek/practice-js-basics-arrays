8c0a7535b7f2e15a4f79084ea27670bcdd96c8cb
Patryk Mleczek <145116649+patrykmleczek@users.noreply.github.com>
2023-11-16 22:01:42 +0100

    solve exercise 04

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,9 @@
 const years = [1980, 1934, 2002, 2019];
+const yearNow = 2023;
+const yearsPast = years.map(howLong);
+
+console.log(yearsPast);
+
+function howLong(element) {
+    return (element -= yearNow) * (-1);
+}
