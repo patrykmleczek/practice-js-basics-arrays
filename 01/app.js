847722e65c8bc0112e322ea47b006c21f4461019
Patryk Mleczek <145116649+patrykmleczek@users.noreply.github.com>
2023-11-16 21:05:56 +0100

    solve exercise 01

---
+++ 01/app.js
@@ -0,0 +1,16 @@
+const users = [
+    'John Doe',
+    'Jane Smith',
+    'Alice Johnson',
+    'Bob Brown',
+    'Emma Williams',
+    'Michael Jones',
+    'Olivia Davis',
+    'William Garcia',
+    'Sophia Martinez',
+    'James Lopez'
+];
+
+console.log(users[0], users[2], users[4]);
+
+console.log('tabilica zawiera: ', users.length, 'elementów');
