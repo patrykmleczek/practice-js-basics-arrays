afd0c08643d930c600aefb957cbfec20417c56ef
Patryk Mleczek <145116649+patrykmleczek@users.noreply.github.com>
2023-11-16 21:22:29 +0100

    solve exercise 02

--- 02/app.js
+++ 02/app.js
@@ -2,6 +2,16 @@ const randomArray = createRandomArray();
 console.log(randomArray);
 
 
+for (let i = 0; i < randomArray.length; i++) {
+    console.log(randomArray[i]);
+}
+console.log('^-- przykład dla for loop');
+
+randomArray.forEach((element) => console.log(element));
+
+console.log('^-- przykład dla .forEach');
+
+
 
 // nie modyfikuj kodu poniżej!
 
@@ -11,13 +21,13 @@ console.log(randomArray);
 function createRandomArray() {
     const arr = [];
     const len = getRandomInteger(1, 10)
-    for(let i=0; i<len; i++) {
-        arr.push( getRandomInteger(1, 100) );
+    for (let i = 0; i < len; i++) {
+        arr.push(getRandomInteger(1, 100));
     }
 
     return arr;
 }
 
 function getRandomInteger(min, max) {
-    return Math.round(Math.random() * (max-min) + min);
+    return Math.round(Math.random() * (max - min) + min);
 }
