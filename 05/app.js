36435278be1ff2f44d4376c9d88095c827bbd9af
Patryk Mleczek <145116649+patrykmleczek@users.noreply.github.com>
2023-11-16 22:24:25 +0100

    solve exercise 05

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,14 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const evenNumbersFilter = numbers.filter(function (element) {
+    return element % 2 === 0
+});
+
+console.log('filter =>', evenNumbersFilter);
+
+const evenNumbersReduce = evenNumbersFilter.reduce(function (acc, currVal) {
+    return acc + currVal;
+}, 0);
+
+console.log('reduce =>', evenNumbersReduce);
+
